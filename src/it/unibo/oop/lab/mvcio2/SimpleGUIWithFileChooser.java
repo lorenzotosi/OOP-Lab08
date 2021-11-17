6aa19d4cb96dec7bc20cc9b1cf78072b21b85f6c
Lorenzo Tosi <lorenzo.tosi10@studio.unibo.it>
2021-11-17 22:58:05 +0100

    added the old interface

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,10 +1,25 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import it.unibo.oop.lab.mvcio.Controller;
+import it.unibo.oop.lab.mvcio.SimpleGUI;
+
 /**
  * A very simple program using a graphical interface.
  * 
  */
-public final class SimpleGUIWithFileChooser {
+public final class SimpleGUIWithFileChooser extends Controller {
 
     /*
      * TODO: Starting from the application in mvcio:
@@ -31,5 +46,45 @@ public final class SimpleGUIWithFileChooser {
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
      */
+    private final JFrame frame = new JFrame();
+    public SimpleGUIWithFileChooser() {
+    final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+    final int sw = (int) screen.getWidth();
+    final int sh = (int) screen.getHeight();
+    frame.setSize(sw / 2, sh / 2);
+    /*
+     * Instead of appearing at (0,0), upper left corner of the screen, this
+     * flag makes the OS window manager take care of the default positioning
+     * on screen. Results may vary, but it is generally the best choice.
+     */
+    frame.setLocationByPlatform(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setTitle("My first graphical interface");
+    final JPanel myPanel = new JPanel();
+    myPanel.setLayout(new BorderLayout());
+    frame.add(myPanel);
+    final JTextArea myTextArea = new JTextArea();
+    myPanel.add(myTextArea);
+    final JButton myButton = new JButton("Save");
+    myPanel.add(myButton, BorderLayout.SOUTH);
+    myButton.addActionListener(new ActionListener() {
 
+        public void actionPerformed(final ActionEvent e) {
+            try {
+                new Controller().writeOnFile(myTextArea.getText());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    });
+    frame.setVisible(true);
+}
+/**
+ * 
+ * @param args ignored
+ * @throws IOException 
+ */
+public static void main(final String[] args) {
+    new SimpleGUI();
+}
 }
