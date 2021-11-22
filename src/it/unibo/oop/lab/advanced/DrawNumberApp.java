2323fca714134e51fb98262b735fb44c803862e7
Lorenzo Tosi <lorenzo.tosi10@studio.unibo.it>
2021-11-22 13:19:34 +0100

    modified constructor

--- src/it/unibo/oop/lab/advanced/DrawNumberApp.java
+++ src/it/unibo/oop/lab/advanced/DrawNumberApp.java
@@ -4,9 +4,9 @@ package it.unibo.oop.lab.advanced;
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
 
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+    //private static final int MIN = 0;
+    //private static final int MAX = 100;
+    //private static final int ATTEMPTS = 10;
     private final DrawNumber model;
     private final DrawNumberView view;
 
@@ -14,10 +14,13 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
      * 
      */
     public DrawNumberApp() {
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+        final SetValues s1 = new SetValues();
+        s1.readFromFile();
+        this.model = new DrawNumberImpl(s1.getMin(), s1.getMax(), s1.getAttempts());
         this.view = new DrawNumberViewImpl();
         this.view.setObserver(this);
         this.view.start();
+        System.out.println(s1.getMax());
     }
 
     @Override
